21effefad7e89a2c851db0124c7bd859389077e2
Love001 <oltyhope2@gmail.com>
2021-08-17 13:11:28 +0100

    first commit

---
+++ precache-manifest.cc2c232594475250366db8ed2c8d0766.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3137bdc59a8bad1addd",
+    "url": "/css/app.456d3d66.css"
+  },
+  {
+    "revision": "dc13706c6eb3e20dd74e",
+    "url": "/css/chunk-1336dad0.fbe1b00e.css"
+  },
+  {
+    "revision": "08704c9191635748089a",
+    "url": "/css/chunk-134b51b1.fbe1b00e.css"
+  },
+  {
+    "revision": "83b68f9fc7f63dd46b7f",
+    "url": "/css/chunk-159ad0d3.fbe1b00e.css"
+  },
+  {
+    "revision": "075cf3eb3bf80dafc6ff",
+    "url": "/css/chunk-15ce82ad.fbe1b00e.css"
+  },
+  {
+    "revision": "16de869fd5c6b8de31ec",
+    "url": "/css/chunk-15e28d4c.fbe1b00e.css"
+  },
+  {
+    "revision": "507737d722d33ab66d5e",
+    "url": "/css/chunk-70fc3a6b.0fa481ee.css"
+  },
+  {
+    "revision": "14a2a66fe63b1e6bdbe9",
+    "url": "/css/chunk-b1efb86e.00c74fb3.css"
+  },
+  {
+    "revision": "3fdcd160032ee354ffc0",
+    "url": "/css/chunk-vendors.a0343f1d.css"
+  },
+  {
+    "revision": "ec7e8b1e44a6681db01e",
+    "url": "/css/projects.df9d9a7c.css"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "66bc62795e869d97a24208bd2410fd5c",
+    "url": "/img/a1.66bc6279.jpg"
+  },
+  {
+    "revision": "81f58a6f1d3c9b63e1373915b7d4204e",
+    "url": "/img/a2.81f58a6f.jpg"
+  },
+  {
+    "revision": "959761fcf0d753afd9453d95f0c690e8",
+    "url": "/img/a3.959761fc.jpg"
+  },
+  {
+    "revision": "94a5b581d93bbab978ec1a32078ff92f",
+    "url": "/img/a4.94a5b581.jpg"
+  },
+  {
+    "revision": "b0fce1ebb99469d7f26e60206422a1a3",
+    "url": "/img/bc12.b0fce1eb.jpg"
+  },
+  {
+    "revision": "62be35a9c12ff67000946cf5ec106c32",
+    "url": "/img/bc14.62be35a9.jpg"
+  },
+  {
+    "revision": "0fbed0173bb6fafa06c69ac1b6717bca",
+    "url": "/img/bible1.0fbed017.jpg"
+  },
+  {
+    "revision": "40f10d16a2615ce02f36e8b7f8b07ba4",
+    "url": "/img/bible2.40f10d16.jpg"
+  },
+  {
+    "revision": "6517eb019355596860bd801653ebc604",
+    "url": "/img/bible3.6517eb01.jpg"
+  },
+  {
+    "revision": "ceb68347e0dfedb358c70c7ba34f0b47",
+    "url": "/img/download.ceb68347.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b1f34be80cdd73602cd1055449078bd5",
+    "url": "/img/parallax2.b1f34be8.jpg"
+  },
+  {
+    "revision": "7bfef9fbc47defa04ef88686d3412358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3137bdc59a8bad1addd",
+    "url": "/js/app.40d59b3f.js"
+  },
+  {
+    "revision": "dc13706c6eb3e20dd74e",
+    "url": "/js/chunk-1336dad0.37314148.js"
+  },
+  {
+    "revision": "08704c9191635748089a",
+    "url": "/js/chunk-134b51b1.43573f69.js"
+  },
+  {
+    "revision": "83b68f9fc7f63dd46b7f",
+    "url": "/js/chunk-159ad0d3.5de083c4.js"
+  },
+  {
+    "revision": "075cf3eb3bf80dafc6ff",
+    "url": "/js/chunk-15ce82ad.efaba3a2.js"
+  },
+  {
+    "revision": "16de869fd5c6b8de31ec",
+    "url": "/js/chunk-15e28d4c.f46c8731.js"
+  },
+  {
+    "revision": "507737d722d33ab66d5e",
+    "url": "/js/chunk-70fc3a6b.9475d85a.js"
+  },
+  {
+    "revision": "14a2a66fe63b1e6bdbe9",
+    "url": "/js/chunk-b1efb86e.54045cd6.js"
+  },
+  {
+    "revision": "3fdcd160032ee354ffc0",
+    "url": "/js/chunk-vendors.50a34f3c.js"
+  },
+  {
+    "revision": "ec7e8b1e44a6681db01e",
+    "url": "/js/projects.740ae798.js"
+  },
+  {
+    "revision": "3fd8b490cbf2a11526eff7076749a4c8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.cc2c232594475250366db8ed2c8d0766.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "churchtemplate"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
